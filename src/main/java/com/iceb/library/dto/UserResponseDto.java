c23a9f2de3cc86cfe2454ed98b5df42047737534
DaviSFS21 <davisfs2110@gmail.com>
2025-02-17 17:42:52 -0300

    feat: start user crud

---
+++ src/main/java/com/iceb/library/dto/UserResponseDto.java
@@ -0,0 +1,24 @@
+package com.iceb.library.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserResponseDto {
+
+    private UUID id;
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+    private List<String> roles;
+
+}
---
+++ src/main/java/com/iceb/library/service/impl/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package com.iceb.library.service.impl;
+
+public class UserServiceImpl {
+}
