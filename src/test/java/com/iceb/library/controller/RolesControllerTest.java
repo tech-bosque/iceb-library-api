3781659fefa592b8117aafe25c76844aded46fd2
Andris Andermann <andris.andermann@gmail.com>
2025-08-19 13:39:10 -0300

    Tests updated with mock auth

--- src/test/java/com/iceb/library/controller/RolesControllerTest.java
+++ src/test/java/com/iceb/library/controller/RolesControllerTest.java
@@ -3,13 +3,18 @@ package com.iceb.library.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.iceb.library.TestUtils;
 import com.iceb.library.dto.RolesResponseDto;
+import com.iceb.library.security.JwtDecoder;
+import com.iceb.library.security.JwtToPrincipalConverter;
 import com.iceb.library.service.RolesService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -17,8 +22,17 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@WithMockUser(roles = "ADMIN")
+@AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles("test")
 @WebMvcTest(RolesController.class)
 public class RolesControllerTest {
+
+    @MockBean
+    private JwtDecoder jwtDecoder;
+
+    @MockBean
+    private JwtToPrincipalConverter jwtToPrincipalConverter;
 
     @Autowired
     private MockMvc mockMvc;
